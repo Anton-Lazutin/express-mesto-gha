10ec54f9d438db946d9c3d8f9ddefe2716d9eb19
Anton Lazutin <laa.63@yandex.ru>
2023-10-06 01:04:43 +0300

    fixed routes cards and signup

--- routes/cards.js
+++ routes/cards.js
@@ -16,7 +16,7 @@ router.post('/', celebrate({
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().required().length(24).hex(),
+    cardId: Joi.string().required().length(24).hex(),
   }),
 }), deleteCards);
 
